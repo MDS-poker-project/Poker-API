7b2db286fa7e30f1338124fe5ba1be240cf30e46
Benjamin.Boutrois <benjamin.boutrois@my-digital-school.org>
2025-05-12 15:44:37 +0200

    fix(main) : Refactor dotenv configuration and improve CORS setup

--- src/main.ts
+++ src/main.ts
@@ -5,23 +5,29 @@ import { Reflector } from '@nestjs/core';
 import { LoggerInterceptorInterceptor } from './logger-interceptor/logger-interceptor.interceptor';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-
 async function bootstrap() {
+  require('dotenv').config();
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalInterceptors(new LoggerInterceptorInterceptor());
-  require('dotenv').config();
-  
-// configuration pour swagger
+  app.enableCors(
+    {
+      origin: process.env.CORS_ORIGIN,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    },
+  );
+
+  // configuration pour swagger
   const config = new DocumentBuilder()
-  .setTitle('Poket API')
-  .setDescription('The Poker API est un des jeus de carte')
-  .setVersion('1.0')
-  .addTag('Poker')
-  .addBearerAuth()
-  .build();
-const documentFactory = () => SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, documentFactory);
-await app.listen(process.env.PORT ?? 3000);
+    .setTitle('Poket API')
+    .setDescription('The Poker API est un des jeus de carte')
+    .setVersion('1.0')
+    .addTag('Poker')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
